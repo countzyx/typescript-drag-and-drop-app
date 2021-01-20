a1b325db8de3295a49d2aa897b3afff9f52322bd
Pedro R. Jiménez <countzyx@gmail.com>
2021-01-19 19:10:20 -0500

    Refactored ProjectInput to Have Less Properties

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,6 @@
 class ProjectInput {
   private _templateElement: HTMLTemplateElement;
   private _hostElement: HTMLDivElement;
-  private _formElement: HTMLFormElement;
-  private _titleInputElement: HTMLInputElement;
-//  private _descriptionInputElement: HTMLInputElement;
-//  private _peopleInputElement: HTMLInputElement;
 
   constructor() {
     this._templateElement = document.getElementById('project-input') as HTMLTemplateElement;
@@ -18,29 +14,28 @@ class ProjectInput {
     }
 
     const importedNode = document.importNode(this._templateElement.content, true);
-    this._formElement = importedNode.firstElementChild as HTMLFormElement;
-    if (!this._formElement) {
+    const formElement = importedNode.firstElementChild as HTMLFormElement;
+    if (!formElement) {
       throw Error('No form element inside template.');
     }
-    this._formElement.id = 'user-input';
-    this._titleInputElement = this._formElement.querySelector('#title') as HTMLInputElement;
-//    this._descriptionInputElement = this._formElement.querySelector('#description') as HTMLInputElement;
-//    this._peopleInputElement = this._formElement.querySelector('#people') as HTMLInputElement;
-    this.configureForm();
-    this.attach();
+    this.configureForm(formElement);
+    this.attach(formElement);
   }
 
-  private attach() {
-    this._hostElement.insertAdjacentElement('afterbegin', this._formElement);
+  private attach(formElement: HTMLFormElement) {
+    this._hostElement.insertAdjacentElement('afterbegin', formElement);
   }
 
-  private configureForm() {
-    this._formElement.addEventListener('submit', this.submitHandler.bind(this));
+  private configureForm(formElement: HTMLFormElement) {
+    formElement.id = 'user-input';
+    formElement.addEventListener('submit', this.submitHandler.bind(this));
   }
 
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this._titleInputElement.value);
+    const formElement = event.target as HTMLFormElement;
+    const titleInputElement = formElement.querySelector('#title') as HTMLInputElement;
+    console.log(titleInputElement.value);
   }
 }
 
